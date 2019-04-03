8b6ce26a8adc3067c14ff65661cef7dd8a969691
root <root@gov.cn>
2019-04-03 21:40:28 +0800

    基本完成

---
+++ app/src/main/java/top/systemsec/survey/activity/NewSurveyActivity.java
@@ -0,0 +1,15 @@
+package top.systemsec.survey.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import top.systemsec.survey.R;
+
+public class NewSurveyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_survey);
+    }
+}
